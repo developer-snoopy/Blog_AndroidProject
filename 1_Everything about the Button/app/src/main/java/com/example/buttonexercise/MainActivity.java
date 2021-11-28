2185e3f733ddceeef477dde4ae5152f90a415b95
DongJaeKwon <kdj9502@naver.com>
2021-11-28 15:04:16 +0900

    Update the Code

--- 1_Everything about the Button/app/src/main/java/com/example/buttonexercise/MainActivity.java
+++ 1_Everything about the Button/app/src/main/java/com/example/buttonexercise/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.buttonexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);   // 레이아웃을 담은 xml 파일에서 정의된 버튼 UI를 정의함
+
 
 
     }
